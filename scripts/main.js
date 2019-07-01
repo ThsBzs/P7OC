41b0eae2fef794c20dae37fce16de486afb1643f
ThsBzs <48803962+ThsBzs@users.noreply.github.com>
2019-07-01 10:54:39 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,8 @@
-let map;
+let map = window.map = new Vue({
+	el: '#map',
 
+	data:{
 
-
-let vm = new Vue({
-	el: '#parisBox',
-
-	data: {
-		shown : true,
-		bienvenue: ''
 	},
 
 	methods: {
@@ -15,11 +10,61 @@ let vm = new Vue({
         	map = new google.maps.Map(document.getElementById('map'), {
           	center: {lat: -34.397, lng: 150.644},
           	zoom: 8
-        });
+        	});
       	},
+	}
+	
+});
+
+/*essai de composants*/
+Vue.component('explain', {
+	/*props: ['type'],*/
+	template: `<div>
+			<p>Pour consulter les restaurants dans votre secteur, centrez la carte sur votre position, ou renseignez une adresse, puis validez. <br>
+			Vous pouvez filtrer les résultats en fonction des notes attribuées par nos utilisateurs.</p> 
+			<br>
+			<p>Si vous souhaitez ajouter un restaurant, cliquez sur "Ajouter", et laissez-vous guider.</p>
+			</div>`
+})
+
+let vm = new Vue({
+	el: '#parisBox',
+	/*components: {explain},*/
+
+	data: {
+		showImgText : true,
+		showAbout: false,
+		showPartners: false,
+		showHome: true,
+		bienvenue: ''
+	},
+
+	methods: {
+		
 		hide(){
-			this.shown == true? this.shown = false : this.shown = true;
-			initMap();
+			this.showImgText == true? this.showImgText = false : this.showImgText = true;
+			showHome = true;
+			map.initMap();
+		},
+		showPageHome(){
+			if (showHome == true){
+				showAbout = false;
+				showPartners = false;
+			};
+		},
+		showPageAbout(){
+			console.log("clickAbout");
+			if (showAbout == true){
+				showPartners = false;
+				showImgText = false;
+			};
+		},
+		showPagePartners(){
+			console.log("clickPartners");
+			if (showPartners == true){
+				showAbout = false;
+				showImgText = false;
+			};
 		},
 
 	}
