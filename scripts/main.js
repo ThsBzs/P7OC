2c59db9964d390cf696b9788de2789888bd61360
ThsBzs <t.brouzes@gmail.com>
2019-07-01 20:26:20 +0200

    Update global

--- scripts/main.js
+++ scripts/main.js
@@ -1,83 +1,113 @@
-let map = window.map = new Vue({
-	el: '#map',
 
-	data:{
+$(function(){
 
-	},
+let showHome = true,
+	showAbout =false,
+	showPartners = false;
 
-	methods: {
-		initMap() {
-        	map = new google.maps.Map(document.getElementById('map'), {
-          	center: {lat: -34.397, lng: 150.644},
-          	zoom: 8
-        	});
-      	},
-	}
-	
+//Premier appel de la fonction pour afficher le contenu dès l'ouverture de la page
+changeDom();
+
+
+$('#home').click(function(e){
+	showHome = true;
+	showAbout = false;
+	showPartners = false;
+	changeDom();
+	e.preventDefault();
 });
 
-/*essai de composants*/
-Vue.component('explain', {
-	/*props: ['type'],*/
-	template: `<div>
-			<p>Pour consulter les restaurants dans votre secteur, centrez la carte sur votre position, ou renseignez une adresse, puis validez. <br>
-			Vous pouvez filtrer les résultats en fonction des notes attribuées par nos utilisateurs.</p> 
-			<br>
-			<p>Si vous souhaitez ajouter un restaurant, cliquez sur "Ajouter", et laissez-vous guider.</p>
-			</div>`
-})
+$('#about').click(function(e){
+	showHome = false;
+	showAbout =true;
+	showPartners = false;
+	changeDom();
+	e.preventDefault();
+});
 
-Vue.component('homepage', {
-	/*props: ['type'],*/
-	template: `<div>
-			<h3>Bienvenue à Paris, capitale mondiale de la gastronomie.</h4>
+$('#partners').click(function(e){
+	showHome = false;
+	showAbout =false;
+	showPartners = true;
+	changeDom();
+	e.preventDefault();
+});
+
+function changeDom(){
+if (showHome == true){
+	$('#parisBox').empty().append(`<div class="col-lg-10 text-center" id="parisImg" v-if="showImgText">
+			<div class="imgText">
+				<h3>Bienvenue à Paris, capitale mondiale de la gastronomie.</h4>
 				<p>Paris Restos vous propose de découvrir les meilleurs restaurants de la capitale, notés par nos utilisateurs, notés par VOUS. <br>
 				Choisissez un secteur, nous vous proposerons les restaurants les plus proches. Vous pouvez par ailleurs contribuer à l'expérience en proposant de nouvelles adresses, qui seront vérifiées par les autres utilisateurs gourmets. <br>
 				</p>
-				<input type="button" class="btn btn-success" value="Commencer" @click.prevent="hide()" @click.prevent="map.initMap()">
-			</div>`
-})
+				<input type="button" class="btn btn-success" value="Commencer" id="begin">
+			</div>
+		</div>`);
+} else if (showAbout == true){
+	$('#parisBox').empty().append(`<div class="col-lg-10 text-center" id="about" v-if="showAbout">
+			<p>A propos de Paris Restos : lorem ipsum...</p>
+		</div>`);
+} else if (showPartners == true){
+	$('#parisBox').empty().append(`<div class="col-lg-10 text-center" id="partners" v-if="showPartners">
+			<p>Nos partenaires : lorem ipsum...</p>
+		</div>`);
+};
+};
+ /*Gestion du clic sur le bouton de lancement*/
+$('#begin').click(function(){
+	$('#parisBox').empty().append(`<div id="explain">
+			<p>Pour consulter les restaurants dans votre secteur, merci d'autoriser l'accès à la position ou la localisation pour votre navigateur. Promis, nous ne viendrons pas sonner chez vous ! <br>
+			Vous pouvez filtrer les résultats en fonction des notes attribuées par nos utilisateurs.</p> 
+			<br>
+			<p>Si vous souhaitez ajouter un restaurant, cliquez sur "Ajouter", et laissez-vous guider.</p>
+			<div id="map"></div>
+			</div>`);
+	initMap();
 
-let vm = new Vue({
-	el: '#parisBox',
-	/*components: {explain},*/
+});
 
-	data: {
-		showImgText : true,
-		showAbout: false,
-		showPartners: false,
-		showHome: true,
-		bienvenue: ''
-	},
 
-	methods: {
-		
-		hide(){
-			this.showImgText == true? this.showImgText = false : this.showImgText = true;
-			showHome = true;
-			map.initMap();
-		},
-		showPageHome(){
-			if (showHome == true){
-				showAbout = false;
-				showPartners = false;
-			};
-		},
-		showPageAbout(){
-			console.log("clickAbout");
-			if (showAbout == true){
-				showPartners = false;
-				showImgText = false;
-			};
-		},
-		showPagePartners(){
-			console.log("clickPartners");
-			if (showPartners == true){
-				showAbout = false;
-				showImgText = false;
-			};
-		},
+/********************GESTION DE LA CARTE*************************/
+let map, infoWindow;
 
-	}
-})
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 48.874955, lng: 2.350517},
+    	zoom: 12
+    });
+    infoWindow = new google.maps.InfoWindow;
 
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        	var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+        	infoWindow.setPosition(pos);
+        	infoWindow.setContent('Vous êtes (à peu près) ici');
+        	infoWindow.open(map);
+        	map.setCenter(pos);
+        }, function() {
+        	handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+
+
+})//Pas touche, fin de funtion jquery
